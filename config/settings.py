21536a1165e93ba47a57ffba9bb2fcd290eacd79
eleenlimon <eleenlimon4@gmail.com>
2023-09-19 15:04:35 -0700

    Fix bug 11

--- config/settings.py
+++ config/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CSRF_TRUSTED_ORIGINS = ["https://limondjango.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://limondjango.up.railway.app/", "https://www.limondjango.up.railway.app/" ]
 
 
 ROOT_URLCONF = "config.urls"
