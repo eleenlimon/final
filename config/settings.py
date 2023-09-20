cbc2fe16eb3e027031323b38ba6917fa3f3f68e8
eleenlimon <eleenlimon4@gmail.com>
2023-09-20 11:07:24 -0700

    oops

--- config/settings.py
+++ config/settings.py
@@ -68,14 +68,6 @@ MIDDLEWARE = [
 
 CSRF_TRUSTED_ORIGINS = ["https://limondjango.up.railway.app/", "http://limondjango.up.railway.app/"]
 
-"""ALLOWED_ORIGINS = [
-    "https://limondjango.up.railway.app/",
-    "https://www.limondjango.up.railway.app/",
-    "http://limondjango.up.railway.app/",
-]
-
-CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS.copy()"""
-
 SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
